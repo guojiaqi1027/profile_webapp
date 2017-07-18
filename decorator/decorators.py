1b20633226d72094ad53a0a2a72f1414d6ad806b
guojiaqi1027 <gawain_rutgers@yahoo.com>
2017-07-18 18:02:18 +0800

    ADD: Decorator for token_required

---
+++ decorator/decorators.py
@@ -0,0 +1,22 @@
+import functools
+from flask import request
+from utils import failure_ret
+from service import query_cache_service
+
+
+def token_required(func):
+    """
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        token = request.values.get('token')
+        if not token:
+            return failure_ret(code=-100, msg='Token is missing')
+
+        uid = query_cache_service.get_uid_by_token(token)
+        if not uid:
+            return failure_ret(code=-101, msg="Token is invalid")
+        else:
+            return func(*args, **kwargs)
+
+    return wrapper
