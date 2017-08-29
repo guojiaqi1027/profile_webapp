eb90a1d9c275b8b5bf48c1599e996678491911ae
guojiaqi1027 <gawain_rutgers@yahoo.com>
2017-08-29 18:35:46 +0800

    ADD: Debug configure for vscode

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from src.api import controller, public_api
 app = Flask(__name__)
 app.register_blueprint(controller)
 app.register_blueprint(public_api)
-if __name__ == '__main__':
+if __name__ == '__main__' or __name__  == 'run':
     app.run(port=9000)
